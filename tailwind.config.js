577205bfb7c3a8edaca3de21b2869c5e908ab8d2
Andrew <andrewrgpires@gmail.com>
2024-09-02 09:46:57 -0300

    config colors / delete old css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{html}"
   ],
   theme: {
-    extend: {},
+    colors: {
+      'black1': '#141414',
+      'orange': '#fe6a0f',
+      'gray1': '#2b2b2b',
+      'gray20': '#5f5f5f',
+    },
   },
   plugins: [],
 }
