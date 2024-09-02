cf0e6d7701603ae372992f34551d5a262661d1e8
Andrew <andrewrgpires@gmail.com>
2024-09-02 14:58:42 -0300

    add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
+    fontFamily: {
+      'sans':['Roboto', 'sans-serif']
+    },
     extend: {
       backgroundImage: {
         "home": "url('../assets/images/bg.png')"
