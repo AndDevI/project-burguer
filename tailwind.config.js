a420eb802186b136cc252b6021664940ac0f6018
Andrew <andrewrgpires@gmail.com>
2024-09-02 10:05:39 -0300

    Revert "config colors / delete old css"
    This reverts commit 577205bfb7c3a8edaca3de21b2869c5e908ab8d2.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,7 @@ module.exports = {
     "./src/**/*.{html}"
   ],
   theme: {
-    colors: {
-      'black1': '#141414',
-      'orange': '#fe6a0f',
-      'gray1': '#2b2b2b',
-      'gray20': '#5f5f5f',
-    },
+    extend: {},
   },
   plugins: [],
 }
